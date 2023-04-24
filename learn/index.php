f1a50c8b1fb0d92707f3c6adc47d4b7c39ca201e
Nedim Basic <basicn86@gmail.com>
2023-04-24 11:29:31 -0400

    Removed excessive links

--- learn/index.php
+++ learn/index.php
@@ -25,33 +25,33 @@
         <div class="learn-left-column">
             <h1 class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></h1>
             <h1 class="learn-section"><a href="./StockTypes.php">2 Types</a></h1>
-            <h2 class="learn-section"><a href="#">2.1 Rule 144</a></h2>
-            <h1 class="learn-section"><a href="#">3 Applications</a></h1>
-            <h2 class="learn-section"><a href="#">3.1 Shareholder Rights</a></h2>
-            <h2 class="learn-section"><a href="#">3.2 Means of financing</a></h2>
-            <h1 class="learn-section"><a href="#">4 Trading</a></h1>
-            <h2 class="learn-section"><a href="#">4.1 Buying</a></h2>
-            <h2 class="learn-section"><a href="#">4.2 Selling</a></h2>
-            <h2 class="learn-section"><a href="#">4.3 Short Selling</a></h2>
-            <h2 class="learn-section"><a href="#">4.3.1 Risks of Short Selling</a></h2>
-            <h2 class="learn-section"><a href="#">4.4 Stock price fluctuations</a></h2>
-            <h2 class="learn-section"><a href="#">4.5 Stock price determination</a></h2>
-            <h2 class="learn-section"><a href="#">4.6 Arbitrate Trading</a></h2>
+            <h2 class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></h2>
+            <h1 class="learn-section">3 Applications</h1>
+            <h2 class="learn-section">3.1 Shareholder Rights</h2>
+            <h2 class="learn-section">3.2 Means of financing</h2>
+            <h1 class="learn-section">4 Trading</h1>
+            <h2 class="learn-section">4.1 Buying</h2>
+            <h2 class="learn-section">4.2 Selling</h2>
+            <h2 class="learn-section">4.3 Short Selling</h2>
+            <h2 class="learn-section">4.3.1 Risks of Short Selling</h2>
+            <h2 class="learn-section">4.4 Stock price fluctuations</h2>
+            <h2 class="learn-section">4.5 Stock price determination</h2>
+            <h2 class="learn-section">4.6 Arbitrate Trading</h2>
         </div>
         <div class="learn-right-column">
-            <h1 class="learn-section"><a href="#">1 What is crypto</a></h1>
-            <h1 class="learn-section"><a href="#">2 Architecture</a></h1>
-            <h2 class="learn-section"><a href="#">2.1 Blockchain</a></h2>
-            <h2 class="learn-section"><a href="#">2.2 Nodes</a></h2>
-            <h2 class="learn-section"><a href="#">2.2.1 Timestamping</a></h2>
-            <h2 class="learn-section"><a href="#">2.3 Mining</a></h2>
-            <h2 class="learn-section"><a href="#">2.4 Wallets</a></h2>
-            <h2 class="learn-section"><a href="#">2.5 Anonymity</a></h2>
-            <h1 class="learn-section"><a href="#">3 Legality</a></h1>
-            <h2 class="learn-section"><a href="#">3.1 Advertising Bans</a></h2>
-            <h2 class="learn-section"><a href="#">3.2 US tax status</a></h2>
-            <h2 class="learn-section"><a href="#">3.3 Loss, theft, &amp; fraud</a></h2>
-            <h2 class="learn-section"><a href="#">3.4 Money Laundering</a></h2>
+            <h1 class="learn-section">1 What is crypto</h1>
+            <h1 class="learn-section">2 Architecture</h1>
+            <h2 class="learn-section">2.1 Blockchain</h2>
+            <h2 class="learn-section">2.2 Nodes</h2>
+            <h2 class="learn-section">2.2.1 Timestamping</h2>
+            <h2 class="learn-section">2.3 Mining</h2>
+            <h2 class="learn-section">2.4 Wallets</h2>
+            <h2 class="learn-section">2.5 Anonymity</h2>
+            <h1 class="learn-section">3 Legality</h1>
+            <h2 class="learn-section">3.1 Advertising Bans</h2>
+            <h2 class="learn-section">3.2 US tax status</h2>
+            <h2 class="learn-section">3.3 Loss, theft, &amp; fraud</h2>
+            <h2 class="learn-section">3.4 Money Laundering</h2>
         </div>
     </div>
 
--- templates/learnSideNav.php
+++ templates/learnSideNav.php
@@ -1,18 +1,18 @@
 <h3 style="text-align: center; text-decoration: underline;">Learn about Stocks&excl;</h3>
 <h3 class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></h3>
 <h3 class="learn-section"><a href="./StockTypes.php">2 Types</a></h3>
-<h4 class="learn-section"><a href="#">2.1 Rule 144</a></h4>
-<h3 class="learn-section"><a href="#">3 Applications</a></h3>
-<h4 class="learn-section"><a href="#">3.1 Shareholder Rights</a></h4>
-<h4 class="learn-section"><a href="#">3.2 Means of financing</a></h4>
-<h3 class="learn-section"><a href="#">4 Trading</a></h3>
-<h4 class="learn-section"><a href="#">4.1 Buying</a></h4>
-<h4 class="learn-section"><a href="#">4.2 Selling</a></h4>
-<h4 class="learn-section"><a href="#">4.3 Short Selling</a></h4>
-<h4 class="learn-section"><a href="#">4.3.1 Risks of Short Selling</a></h4>
-<h4 class="learn-section"><a href="#">4.4 Stock price fluctuations</a></h4>
-<h4 class="learn-section"><a href="#">4.5 Stock price determination</a></h4>
-<h4 class="learn-section"><a href="#">4.6 Arbitrate Trading</a></h4>
+<h4 class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></h4>
+<h3 class="learn-section">3 Applications</h3>
+<h4 class="learn-section">3.1 Shareholder Rights</h4>
+<h4 class="learn-section">3.2 Means of financing</h4>
+<h3 class="learn-section">4 Trading</h3>
+<h4 class="learn-section">4.1 Buying</h4>
+<h4 class="learn-section">4.2 Selling</h4>
+<h4 class="learn-section">4.3 Short Selling</h4>
+<h4 class="learn-section">4.3.1 Risks of Short Selling</h4>
+<h4 class="learn-section">4.4 Stock price fluctuations</h4>
+<h4 class="learn-section">4.5 Stock price determination</h4>
+<h4 class="learn-section">4.6 Arbitrate Trading</h4>
 
 <div style="height: 50px;"></div> <!--Spacer-->
 
