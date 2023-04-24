48cf57e2ddbdb8cd330d3f4e1c1a1d1b4188170d
Nedim Basic <basicn86@gmail.com>
2023-04-24 14:05:04 -0400

    Added accessibility features to the side nav

--- templates/learnSideNav.php
+++ templates/learnSideNav.php
@@ -1,21 +1,23 @@
-<span style="display: block; font-weight: bold; text-align: center; text-decoration: underline;">Learn about Stocks&excl;</span>
-<span class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></span>
-<span class="learn-section"><a href="./StockTypes.php">2 Types</a></span>
-<span class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></span>
-<span class="learn-section">3 Applications</span>
-<span class="learn-section">3.1 Shareholder Rights</span>
-<span class="learn-section">3.2 Means of financing</span>
-<span class="learn-section">4 Trading</span>
-<span class="learn-section">4.1 Buying</span>
-<span class="learn-section">4.2 Selling</span>
-<span class="learn-section">4.3 Short Selling</span>
-<span class="learn-section">4.3.1 Risks of Short Selling</span>
-<span class="learn-section">4.4 Stock price fluctuations</span>
-<span class="learn-section">4.5 Stock price determination</span>
-<span class="learn-section">4.6 Arbitrate Trading</span>
+<ul>
+<li style="display: block; font-weight: bold; text-align: center; text-decoration: underline;">Learn about Stocks&excl;</li>
+<li class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></li>
+<li class="learn-section"><a href="./StockTypes.php">2 Types</a></li>
+<li class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></li>
+<li class="learn-section">3 Applications</li>
+<li class="learn-section">3.1 Shareholder Rights</li>
+<li class="learn-section">3.2 Means of financing</li>
+<li class="learn-section">4 Trading</li>
+<li class="learn-section">4.1 Buying</li>
+<li class="learn-section">4.2 Selling</li>
+<li class="learn-section">4.3 Short Selling</li>
+<li class="learn-section">4.3.1 Risks of Short Selling</li>
+<li class="learn-section">4.4 Stock price fluctuations</li>
+<li class="learn-section">4.5 Stock price determination</li>
+<li class="learn-section">4.6 Arbitrate Trading</li>
 
-<div style="height: 50px;"></div> <!--Spacer-->
+<div style="height: 50px;" aria-hidden="true"></div> <!--Spacer-->
 
-<span style="display: block; text-align: center; text-decoration: underline;">Regulations</span>
-<span class="learn-section"><a href="https://www.finra.org/#/">Finra Regulations &lpar;Finra.org&rpar;</a></span>
-<span class="learn-section"><a href="https://www.sec.gov/">Securities and Exchange commission &lpar;SEC.gov&rpar;</a></span>
+<li style="display: block; text-align: center; text-decoration: underline;">Regulations</li>
+<li class="learn-section"><a href="https://www.finra.org/#/">Finra Regulations &lpar;Finra.org&rpar;</a></li>
+<li class="learn-section"><a href="https://www.sec.gov/">Securities and Exchange commission &lpar;SEC.gov&rpar;</a></li>
+</ul>
