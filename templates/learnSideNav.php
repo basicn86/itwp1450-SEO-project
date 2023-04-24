f26ca47a83d17dbe043b7f100cd39fa169df6c09
Nedim Basic <basicn86@gmail.com>
2023-04-24 13:56:25 -0400

    Optimized headings

--- templates/learnSideNav.php
+++ templates/learnSideNav.php
@@ -1,21 +1,21 @@
-<h3 style="text-align: center; text-decoration: underline;">Learn about Stocks&excl;</h3>
-<h3 class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></h3>
-<h3 class="learn-section"><a href="./StockTypes.php">2 Types</a></h3>
-<h4 class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></h4>
-<h3 class="learn-section">3 Applications</h3>
-<h4 class="learn-section">3.1 Shareholder Rights</h4>
-<h4 class="learn-section">3.2 Means of financing</h4>
-<h3 class="learn-section">4 Trading</h3>
-<h4 class="learn-section">4.1 Buying</h4>
-<h4 class="learn-section">4.2 Selling</h4>
-<h4 class="learn-section">4.3 Short Selling</h4>
-<h4 class="learn-section">4.3.1 Risks of Short Selling</h4>
-<h4 class="learn-section">4.4 Stock price fluctuations</h4>
-<h4 class="learn-section">4.5 Stock price determination</h4>
-<h4 class="learn-section">4.6 Arbitrate Trading</h4>
+<span style="display: block; font-weight: bold; text-align: center; text-decoration: underline;">Learn about Stocks&excl;</span>
+<span class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></span>
+<span class="learn-section"><a href="./StockTypes.php">2 Types</a></span>
+<span class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></span>
+<span class="learn-section">3 Applications</span>
+<span class="learn-section">3.1 Shareholder Rights</span>
+<span class="learn-section">3.2 Means of financing</span>
+<span class="learn-section">4 Trading</span>
+<span class="learn-section">4.1 Buying</span>
+<span class="learn-section">4.2 Selling</span>
+<span class="learn-section">4.3 Short Selling</span>
+<span class="learn-section">4.3.1 Risks of Short Selling</span>
+<span class="learn-section">4.4 Stock price fluctuations</span>
+<span class="learn-section">4.5 Stock price determination</span>
+<span class="learn-section">4.6 Arbitrate Trading</span>
 
 <div style="height: 50px;"></div> <!--Spacer-->
 
-<h3 style="text-align: center; text-decoration: underline;">Regulations</h3>
-<h3 class="learn-section"><a href="https://www.finra.org/#/">Finra Regulations &lpar;Finra.org&rpar;</a></h3>
-<h3 class="learn-section"><a href="https://www.sec.gov/">Securities and Exchange commission &lpar;SEC.gov&rpar;</a></h3>
+<span style="display: block; text-align: center; text-decoration: underline;">Regulations</span>
+<span class="learn-section"><a href="https://www.finra.org/#/">Finra Regulations &lpar;Finra.org&rpar;</a></span>
+<span class="learn-section"><a href="https://www.sec.gov/">Securities and Exchange commission &lpar;SEC.gov&rpar;</a></span>
