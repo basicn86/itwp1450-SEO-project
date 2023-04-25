66e553f9cdf79573d93b3094e397568e70fa6e60
Nedim Basic <basicn86@gmail.com>
2023-04-25 04:06:57 -0400

    Navigation optimizations

--- templates/footernav.php
+++ templates/footernav.php
@@ -4,24 +4,27 @@
 <a href="https://twitter.com/"><img src="/itwp1450/images/icons/twitter.png" width="64" height="64" alt="Follow on Twitter"></a>
 <a href="https://www.reddit.com/"><img src="/itwp1450/images/icons/reddit.png" width="64" height="64" alt="Follow on Reddit"></a>
 </div>
-<ul class="footer-col1">
-	<li><a href="#">Careers</a></li>
-	<li><a href="#">Newsletter</a></li>
-	<li><a href="#">Accessibility</a></li>
-	<li><a href="#">Investors</a></li>
-	<li><a href="#">Press</a></li>
-</ul>
-<ul class="footer-col2">
-	<li><a href="#">List Stocks</a></li>
-	<li><a href="#">Affiliates</a></li>
-	<li><a href="#">Advertising</a></li>
-	<li><a href="#">Publishing</a></li>
-	<li><a href="#">Business Relations</a></li>
-</ul>
-<ul class="footer-col3">
-	<li><a href="#">Privacy Policy</a></li>
-	<li><a href="#">Tax Info</a></li>
-	<li><a href="#">Disputes</a></li>
-	<li><a href="#">Fees</a></li>
-	<li><a href="#">Legal Info</a></li>
-</ul>
+
+<nav>
+	<ul class="footer-col1">
+		<li><a href="#">Careers</a></li>
+		<li><a href="#">Newsletter</a></li>
+		<li><a href="#">Accessibility</a></li>
+		<li><a href="#">Investors</a></li>
+		<li><a href="#">Press</a></li>
+	</ul>
+	<ul class="footer-col2">
+		<li><a href="#">List Stocks</a></li>
+		<li><a href="#">Affiliates</a></li>
+		<li><a href="#">Advertising</a></li>
+		<li><a href="#">Publishing</a></li>
+		<li><a href="#">Business Relations</a></li>
+	</ul>
+	<ul class="footer-col3">
+		<li><a href="#">Privacy Policy</a></li>
+		<li><a href="#">Tax Info</a></li>
+		<li><a href="#">Disputes</a></li>
+		<li><a href="#">Fees</a></li>
+		<li><a href="#">Legal Info</a></li>
+	</ul>
+</nav>
--- templates/topnav.php
+++ templates/topnav.php
@@ -1,13 +1,15 @@
 <img class="companylogo" src="/itwp1450/images/logoplaceholder.png" width="1515" height="300" alt="BoneStocks company logo">
-<ul class="navbar">
-    <li><a href="/itwp1450/index.php">Home</a></li>
-    <li>News</li>
-    <li><a href="/itwp1450/learn/">Learn</a></li>
-    <li>Stocks</li>
-    <li>Crypto</li>
-    <li>Support</li>
-    <li>Contact</li>
-</ul>
+<nav>
+    <ul class="navbar">
+        <li><a href="/itwp1450/index.php">Home</a></li>
+        <li>News</li>
+        <li><a href="/itwp1450/learn/">Learn</a></li>
+        <li>Stocks</li>
+        <li>Crypto</li>
+        <li>Support</li>
+        <li>Contact</li>
+    </ul>
+</nav>
 <form class="search" method="post">
     <p><input type="text" name="search_term" size="64" value="<?php if(isset($_POST['search_term'])) {echo $_POST['search_term'];} ?>"></p>
     <p><input class="submit-button" type="submit" name="submit" value="Search&excl;"></p>
