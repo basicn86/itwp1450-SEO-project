ebf521b119fd0f39a977b33d2757d88b8a8acce2
Nedim Basic <basicn86@gmail.com>
2023-04-26 11:22:18 -0400

    Fixed an HTML error

--- templates/learnSideNav.php
+++ templates/learnSideNav.php
@@ -1,6 +1,6 @@
 <nav>
+    <h2 style="display: block; font-weight: bold; text-align: center; text-decoration: underline;">Learn about Stocks&excl;</h2>
     <ul>
-        <h2 style="display: block; font-weight: bold; text-align: center; text-decoration: underline;">Learn about Stocks&excl;</h2>
         <li class="learn-section"><a href="./WhatAreStocks.php">1 What are stocks</a></li>
         <li class="learn-section"><a href="./StockTypes.php">2 Types</a></li>
         <li class="learn-section"><a href="./Rule144.php">2.1 Rule 144</a></li>
@@ -15,10 +15,13 @@
         <li class="learn-section">4.4 Stock price fluctuations</li>
         <li class="learn-section">4.5 Stock price determination</li>
         <li class="learn-section">4.6 Arbitrate Trading</li>
+    </ul>
 
         <div style="height: 50px;" aria-hidden="true"></div> <!--Spacer-->
 
         <h2 style="display: block; text-align: center; text-decoration: underline;">Regulations</h2>
+    
+    <ul>
         <li class="learn-section"><a href="https://www.finra.org/#/">Finra Regulations &lpar;Finra.org&rpar;</a></li>
         <li class="learn-section"><a href="https://www.sec.gov/">Securities and Exchange commission &lpar;SEC.gov&rpar;</a></li>
     </ul>
